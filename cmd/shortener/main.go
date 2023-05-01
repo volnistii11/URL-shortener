0988b5a1e469b5986d4102bde54889bf30f09465
volnistii <cor.fascio@gmail.com>
2023-05-01 12:18:30 +0300

    first inc with second endpoint

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,11 +5,13 @@ import (
 	"io"
 	"math/rand"
 	"net/http"
+	"strings"
 	"time"
 )
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
+	urlMap = map[string]string{}
 }
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
@@ -24,29 +26,56 @@ func randString(n int) string {
 	return string(b)
 }
 
-func mainPage(w http.ResponseWriter, r *http.Request) {
+func mainHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		if err := r.ParseForm(); err != nil {
-			w.Write([]byte(err.Error()))
-			return
-		}
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			fmt.Println(err)
-		}
-		r.Body.Close()
+		createShortURL(w, r)
+	} else if r.Method == http.MethodGet {
+		getFullURL(w, r)
+	}
+}
 
-		urlMap[string(body)] = r.Host + r.RequestURI + randString(10)
+func createShortURL(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	r.Body.Close()
+	if len(body) == 0 {
+		http.Error(w, "Body is empty.", http.StatusBadRequest)
+		return
+	}
+	scheme := "http"
+	if r.TLS != nil {
+		scheme = "https"
+	}
+	shortURL := randString(10)
+	urlMap[shortURL] = string(body)
 
-		w.Write([]byte(urlMap[string(body)]))
+	w.Header().Set("content-type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("%v://%v%v%v", scheme, r.Host, r.RequestURI, shortURL)))
+}
+
+func getFullURL(w http.ResponseWriter, r *http.Request) {
+	path := strings.Trim(r.URL.Path, "/")
+	shortURL := strings.Split(path, "/")[0]
+
+	if fullURL, ok := urlMap[shortURL]; ok {
+		w.Header().Set("Location", fullURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
 	}
 }
 
 func main() {
-	urlMap = map[string]string{}
-
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, mainPage)
+	mux.HandleFunc(`/`, mainHandler)
 
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -2,10 +2,4 @@ module github.com/volnistii11/URL-shortener
 
 go 1.19
 
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/stretchr/testify v1.8.2 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-)
+require github.com/gorilla/mux v1.8.0
