141b0ebbc7f1ca93a8350ed99dedbf85a5301044
volnistii <cor.fascio@gmail.com>
2023-05-03 22:52:10 +0300

    second inc, attempt 2

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,1 +1,16 @@
 package server
+
+import (
+	"github.com/volnistii11/URL-shortener/internal/app/handlers"
+	"net/http"
+)
+
+func RunServer() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, handlers.MainHandler)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -1,1 +1,7 @@
 package storage
+
+var URLMap map[string]string
+
+//type URLStorage struct {
+//	URLMap map[string]string
+//}
