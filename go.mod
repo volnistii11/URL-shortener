817ecf2b92bdfd0607e7ec53767be686db1b136e
volnistii <cor.fascio@gmail.com>
2023-05-08 21:17:35 +0300

    fifth increment

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/gorilla/mux v1.8.0
 
 require (
 	github.com/bytedance/sonic v1.8.8 // indirect
+	github.com/caarlos0/env v3.5.0+incompatible // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
--- internal/app/config/flags.go
+++ internal/app/config/flags.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var Addresses struct {
 	Server           string
@@ -8,8 +11,14 @@ var Addresses struct {
 }
 
 func ParseFlags() {
-
 	flag.StringVar(&Addresses.Server, "a", ":8080", "server address")
 	flag.StringVar(&Addresses.RespondingServer, "b", "", "responding server address")
 	flag.Parse()
+
+	if envServerAddress := os.Getenv("SERVER_ADDRESS"); envServerAddress != "" {
+		Addresses.Server = envServerAddress
+	}
+	if envRespondingServerAddress := os.Getenv("RESPONDING_SERVER_ADDRESS"); envRespondingServerAddress != "" {
+		Addresses.RespondingServer = envRespondingServerAddress
+	}
 }
