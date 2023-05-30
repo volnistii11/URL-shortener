836fdb9dc56204452263f1d056e427680d574300
volnistii <cor.fascio@gmail.com>
2023-05-30 23:55:41 +0300

    Refactor shortenertest errors

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/volnistii11/URL-shortener/internal/app/config"
 	"github.com/volnistii11/URL-shortener/internal/app/server"
 	"github.com/volnistii11/URL-shortener/internal/app/storage"
@@ -13,13 +14,19 @@ func main() {
 	repo := storage.NewRepository()
 	cfg := config.NewFlags()
 	cfg.ParseFlags()
-	fileStorage := file.NewSetter(repo, cfg)
 	logger, err := telemetry.NewLogger()
 	if err != nil {
 		log.Printf("Error : %v\n", err)
 	}
 	defer logger.Sync()
 	r := server.NewRouter(logger)
-	s := r.Router(fileStorage.RestoreDataFromJSONFileToStructure(), cfg)
+
+	var s *gin.Engine
+	if cfg.GetFileStoragePath() != "" {
+		fileStorage := file.NewSetter(repo, cfg)
+		s = r.Router(fileStorage.RestoreDataFromJSONFileToStructure(), cfg)
+	} else {
+		s = r.Router(repo, cfg)
+	}
 	s.Run(cfg.GetServer())
 }
