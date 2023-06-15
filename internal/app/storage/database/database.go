f1d0312483dd0818d090c727d70958f8a7f87918
volnistii <cor.fascio@gmail.com>
2023-06-15 23:04:25 +0300

    Delete unique index from correlation_id

--- internal/app/storage/database/database.go
+++ internal/app/storage/database/database.go
@@ -32,7 +32,7 @@ func (db *database) CreateTableIfNotExists() error {
 	}
 
 	_, err := db.repo.GetDatabase().
-		Exec("CREATE TABLE IF NOT EXISTS url_dependencies (id serial primary key, correlation_id varchar(255) null unique , short_url varchar(255) not null unique, original_url varchar(255) not null unique)")
+		Exec("CREATE TABLE IF NOT EXISTS url_dependencies (id serial primary key, correlation_id varchar(255) null, short_url varchar(255) not null unique, original_url varchar(255) not null unique)")
 	if err != nil {
 		return err
 	}
