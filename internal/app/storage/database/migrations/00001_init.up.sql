97499e56e6ee7d2ddc1e5bf4725d750a707c5b76
volnistii11 <cor.fascio@gmail.com>
2023-07-02 20:42:12 +0300

    Change database schema

--- internal/app/storage/database/migrations/00001_init.up.sql
+++ internal/app/storage/database/migrations/00001_init.up.sql
@@ -1,8 +1,13 @@
+BEGIN;
 CREATE TABLE IF NOT EXISTS url_dependencies
 (
-    id             serial primary key,
-    short_url      varchar(255) not null unique,
-    original_url   varchar(255) not null,
-    user_id        integer      null unique,
-    is_deleted     boolean      default false
+    id           serial primary key,
+    short_url    varchar(255) not null unique,
+    original_url varchar(255) not null,
+    user_id      integer      null,
+    is_deleted   boolean default false
 );
+
+CREATE INDEX url_dependencies_user_id_idx on url_dependencies (user_id);
+CREATE INDEX url_dependencies_is_deleted_idx on url_dependencies(is_deleted);
+COMMIT;
