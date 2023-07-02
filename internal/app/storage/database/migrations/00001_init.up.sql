416f4d1e2018bce40eb6fbeb5cede3e8ad025e7e
volnistii11 <cor.fascio@gmail.com>
2023-07-02 20:53:39 +0300

    Change database schema

--- internal/app/storage/database/migrations/00001_init.up.sql
+++ internal/app/storage/database/migrations/00001_init.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS url_dependencies
 (
     id           serial primary key,
     short_url    varchar(255) not null unique,
-    original_url varchar(255) not null,
+    original_url varchar(255) not null unique,
     user_id      integer      null,
     is_deleted   boolean default false
 );
